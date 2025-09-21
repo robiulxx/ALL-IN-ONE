7ebece108097a72aa6dbf4baf78385832b32114a
robiulxx <86838778+robiulxx@users.noreply.github.com>
2025-09-22 00:07:08 +0600

    config.py

--- config.py
+++
@@ -1,3 +0,0 @@
-# Replace these with your actual API details
-COMMAND_PREFIX = ["/", ".", "!", ","]    #Used Just For Proffessionalism In Bot 
-PROFILE_ERROR_URL = "https://t.me/allccgen_bot"  #Replace With Your Desired One 
