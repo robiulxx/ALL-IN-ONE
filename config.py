7572fe459ee2ac46184dfa9a17ec48e7cf130fdf
robiulxx <86838778+robiulxx@users.noreply.github.com>
2025-09-21 23:31:01 +0600

    Add files via upload

---
+++ config.py
@@ -0,0 +1,3 @@
+# Replace these with your actual API details
+COMMAND_PREFIX = ["/", ".", "!", ","]    #Used Just For Proffessionalism In Bot 
+PROFILE_ERROR_URL = "https://t.me/allccgen_bot"  #Replace With Your Desired One 
